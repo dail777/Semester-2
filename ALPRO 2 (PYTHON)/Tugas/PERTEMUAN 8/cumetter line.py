ac9c76411df1cebb5ee9a3393251ea907214cd77
wawan-maulana <wawanme07@gmail.com>
2026-04-04 13:52:36 +0700

    comutter line

---
+++ ALPRO 2 (PYTHON)/Tugas/PERTEMUAN 8/cumetter line.py
@@ -0,0 +1,27 @@
+#login / register
+#topup - membeli member 
+#rute - kalo member = diskon
+
+uang = 0
+member = False
+
+def top_up(saldo_saat_ini):
+    jumlah = int(input("Masukkan jumlah top up: "))
+    return saldo_saat_ini + jumlah
+
+def member():
+    print("pilih member :\n 1. bronze\n 2. silver\n 3. gold")
+    member = int(input("pilih member :"))
+    if member == 1 :
+        print("member bronze Rp : 20K")
+    elif member == 2 :
+        print("member silver Rp : 30K")
+    elif member == 3 :
+        print("member gold Rp : 50K")
+    else :
+        print("error")
+
+# Update nilai uang dengan hasil dari fungsi
+#uang = top_up(uang)
+#print(f"Saldo sekarang: {uang}")
+member()
